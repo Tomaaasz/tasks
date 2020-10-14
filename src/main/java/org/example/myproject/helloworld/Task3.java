db576bd6e70f7e3b0debabb6fbbe2eeab850460e
Musza <mechanicalforest@gmail>
2020-10-14 13:01:42 +0200

    Implement Task3

---
+++ src/main/java/org/example/myproject/helloworld/Task3.java
@@ -0,0 +1,7 @@
+package org.example.myproject.helloworld;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("1234".length() == "2345".length());
+    }
+}
